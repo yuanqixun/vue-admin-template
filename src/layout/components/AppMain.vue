6e64605339954e217b72820852ad361e5c53db84
袁启勋 <xun.yuan@infoship.cn>
2020-02-04 20:57:09 +0800

    去掉动画过渡效果。

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,10 +1,8 @@
 <template>
   <section class="app-main">
-    <transition name="fade-transform" mode="out-in">
-      <keep-alive :include="cachedViews">
-        <router-view :key="key" :vname="key2" />
-      </keep-alive>
-    </transition>
+    <keep-alive :include="cachedViews">
+      <router-view :key="key" :vname="key2" />
+    </keep-alive>
   </section>
 </template>
 
